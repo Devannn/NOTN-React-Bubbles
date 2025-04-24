f5ed2149c8c7359be40e14d3c3536694a449772c
Devan <DevanJanssen@gmail.com>
2025-04-24 13:23:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BubbleStats from './BubbleStats';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BubbleStats />
+    </div>
+  );
+}
+
+export default App;
